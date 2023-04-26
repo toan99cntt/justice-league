42bcac315c153cba77cd1c465c48e0b6b4d631ff
toan.nguyen-99 <toan.nguyen-99@pirago.work>
2023-04-26 09:30:49 +0700

    no msg

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,7 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <main-header />
 
-    <q-page-container >
+    <q-page-container style="padding-top: 0">
       <q-page class="main-content" :class="{ 'bg-login': isLayoutLogin }">
         <div class="container">
           <router-view />
@@ -26,7 +26,8 @@ const isLayoutLogin = computed(() => route.name === 'login' || route.name === 'r
 .main-content {
   background-color: #f8f9fd;
   .container {
-    height: calc(100vh - 65px);
+    height: 100vh;
+    padding-top: 65px;
     width: 100%;
     background: #fff;
   }
